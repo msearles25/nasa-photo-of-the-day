78f6d3251056a0ffc733096935d397adb2160bc9
msearles25 <skaman4459@gmail.com>
2019-11-07 16:02:46 -0500

    header component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./App.css";
 
 import ApodGrid from './components/ApodGrid';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        NASA Photo of the day!🚀
-      </h1>
+      <Header />
       <ApodGrid />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <h1>NASA Photo of the day!<span role='image' aria-label='rocket ship emoji'>🚀</span></h1>
+    )
+}
+
+export default Header;
